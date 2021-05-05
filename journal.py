5f55f73c9fd32ccecc078d6e79807cfaa32c4e64
William <s3864077@rmit.edu.vn>
2021-05-06 00:22:32 +0700

    Testing re library

--- journal.py
+++ journal.py
@@ -1,21 +1,26 @@
 from time import gmtime, strftime
+import re
+
 
 def journal():
     content = ""
     infile = open("account.txt", "w")
-    infile.write(str(strftime("%a, %d %b %Y %H:%M:%S +0000 \n", gmtime()) + ' '))
-    print("Input text: ", end='')
+    infile.write(str(strftime("%a, %d %b %Y %H:%M:%S +0000,\n", gmtime())))
+    print("Input text: ")
     while 1:
         content = input()
-        infile.write("\n")
         if content == "exit":
             break
-        infile.write(content)
-
-
-
-
+        infile.write(content + '\n')
     infile.close()
 
 
-journal()
+def read():
+    infile = open("account.txt", "r")
+    word = infile.read()
+    split = re.split('\n|,', word)
+    # split = word.split(',')
+    print(split)
+
+
+read()
