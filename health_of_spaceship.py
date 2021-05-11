6f81b53b8778e5ee9379313f12d602449e249d23
quangan186 <quangan186@gmail.com>
2021-05-11 17:26:57 +0700

    An push 11/5 (comment)

--- health_of_spaceship.py
+++ health_of_spaceship.py
@@ -1,6 +1,8 @@
 import random
 
-health_of_spaceship = 100
+health_of_spaceship = 100  # %
+
+# dictionary of problems and damage
 problems = {
     "fan": 20,
     "heat": 30,
@@ -8,9 +10,9 @@ problems = {
     "radar": 40,
 }
 for name, damage in problems.items():
-    if random.random() < 0.5:
+    if random.random() < 0.5:  # random the possibility of problems happen
         continue
-    print(name)
+    print(name)  # print problems
     health_of_spaceship -= damage
     print(f'current_health: {health_of_spaceship}')
     if health_of_spaceship <= 0:
