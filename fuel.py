ad27ab2548a071256b1b1786da555f5d47dd2a59
quangan186 <quangan186@gmail.com>
2021-05-11 16:36:07 +0700

    An push 11/5 (comment)

--- fuel.py
+++ fuel.py
@@ -1,17 +1,22 @@
+
 import time
 
 
-def fuel_cal(a, b):
-    current_fuel_level = a
-    fuel_burn_rate = b
-    print(f'Current fuel level: {current_fuel_level}')
-    while True:
+def fuel_cal(current_fuel_level, fuel_burn_rate):
+    """
+    Calculate the current fuel level of spaceship
+    :param current_fuel_level: current fuel level
+    :param fuel_burn_rate: fuel burn rate
+    :return: the change of spaceship's fuel
+    """
+
+    print(f'Current fuel level: {current_fuel_level} liters')
+    while current_fuel_level != 0:
         current_fuel_level -= fuel_burn_rate
-        time.sleep(1)  # delay 1 second
-        print(f'Current fuel: {current_fuel_level}')
-        if current_fuel_level == 0:
-            print('Out of energy')
-            break
+        time.sleep(1)  # delay 60 seconds
+        print(f'Current fuel: {current_fuel_level} liters')
+    else:
+        print('Out of energy')
 
 
 fuel_cal(100, 10)
