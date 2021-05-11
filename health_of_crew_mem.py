578706db9823c8118fcdd35a72164237e553bdfa
quangan186 <quangan186@gmail.com>
2021-05-11 17:19:06 +0700

    An push 11/5 (comment and fix code)

--- health_of_crew_mem.py
+++ health_of_crew_mem.py
@@ -1,16 +1,32 @@
 import random
 
 
-def health_of_crew_members(n):
-    health_problem_list = ['fever', 'sneeze', 'stomachache', 'none', 'sore throat']
-    crew_numbers = random.randrange(1, n)
-    i = random.choice(health_problem_list)
-    if crew_numbers >= 2:
-        print(f'There are {n} total members  and {crew_numbers} of them have {i}')
+def health_of_crew_members(number_of_members):
+    """
+    Define the number of members and define how many victims are there and their symptoms
+    :param number_of_members: the quantity of members in spaceship
+    :return: the number of members, number of victims and their symptoms in spaceship
+    """
+    # list of symptoms
+    health_problem_list = ['fever', 'sneeze', 'stomachache', 'sore throat']
+
+    # random the number of victims
+    victims = random.randrange(0, number_of_members)
+
+    # random the symptom
+    problem = random.choice(health_problem_list)
+
+    # if the number of victims larger than 2 and have symptom
+    if victims >= 2:
+        print(f'There are {number_of_members} total members and {victims} members have {problem}')
+
+    # if the number of victims equal to 1 and have symptom
+    elif victims == 1:
+        print(f'There are {number_of_members} total members and {victims} member has {problem}')
+
+    # if no one has symptom
     else:
-        print(f'There are {n} total members  and {crew_numbers} member has {i}')
-    if i == 'none':
-        print(f'There are {n} total members  and all of them are normal')
+        print(f'There are {number_of_members} total members and all of them are normal')
 
 
 health_of_crew_members(10)
