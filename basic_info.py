ff969ce60751f6d1e1ee6e0c959d4d9fbce64e19
quangan186 <quangan186@gmail.com>
2021-05-19 20:48:51 +0700

    An push 19/5 (menu + basic info)

--- basic_info.py
+++ basic_info.py
@@ -1,3 +1,4 @@
+
 import random
 from datetime import datetime
 import time
@@ -10,14 +11,6 @@ t0 = time.mktime(d.timetuple())
 
 
 def distance_and_time_cal(s, v):
-    """
-    Calculate distance from Earth to current spaceship location, current spaceship location to Mars and
-the estimated time to go to Mars
-    :param s: distance between Earth and Mars in kilometers
-    :param v: current velocity of spaceship in kilometers/secs
-    :return: distance from Earth to current spaceship location, current spaceship location to Mars and
-the estimated time to go to Mars
-    """
     print(f'Distance between Earth and Mars: {s} km')
     print(f'Spaceship velocity: {v} km/s')
     while True:
@@ -25,14 +18,12 @@ the estimated time to go to Mars
         named_tuple = time.localtime()  # get struct_time
         time_string = time.strftime("%d/%m/%Y, %H:%M:%S", named_tuple)
         print("\nTime now {}".format(time_string))
-        t1 = time.time()  # current time
+        t1 = time.time()
         delta_time_seconds = t1 - t0
 
-        # distance from Earth
         distance_from_Earth = v * delta_time_seconds
         print(f'Current distance from spaceship to Earth: {distance_from_Earth} km')
 
-        # distance from Mars
         distance_from_Mars = s - delta_time_seconds * v
         print(f'Current distance from spaceship to Mars: {distance_from_Mars} km')
 
@@ -48,46 +39,29 @@ the estimated time to go to Mars
 
 
 def fuel_cal(current_fuel_level, fuel_burn_rate):
-    """
-    Calculate the current fuel level of spaceship
-    :param current_fuel_level: current fuel level
-    :param fuel_burn_rate: fuel burn rate
-    :return: the change of spaceship's fuel
-    """
 
     print(f'Current fuel level: {current_fuel_level} liters')
     while current_fuel_level != 0:
         current_fuel_level -= fuel_burn_rate
-        time.sleep(60)  # delay 60 seconds
+        time.sleep(60)
         print(f'Current fuel: {current_fuel_level} liters')
     else:
         print('Out of energy')
 
 
 def health_of_crew_members(number_of_members):
-    """
-    Define the number of members and define how many victims are there and their symptoms
-    :param number_of_members: the quantity of members in spaceship
-    :return: the number of members, number of victims and their symptoms in spaceship
-    """
-    # list of symptoms
     health_problem_list = ['fever', 'sneeze', 'stomachache', 'sore throat']
     while True:
-        # random the number of victims
         victims = random.randrange(0, number_of_members)
 
-        # random the symptom
         problem = random.choice(health_problem_list)
 
-        # if the number of victims larger than 2 and have symptom
         if victims >= 2:
             print(f'There are {number_of_members} total members and {victims} members have {problem}')
 
-        # if the number of victims equal to 1 and have symptom
         elif victims == 1:
             print(f'There are {number_of_members} total members and {victims} member has {problem}')
 
-        # if no one has symptom
         else:
             print(f'There are {number_of_members} total members and all of them are normal')
         time.sleep(60)
@@ -96,9 +70,9 @@ def health_of_crew_members(number_of_members):
 def spaceship_health(health_of_spaceship, problems):
     print(f'Health of spaceship: {health_of_spaceship}')
     for name, damage in problems.items():
-        if random.random() < 0.5:  # random the possibility of problems happen
+        if random.random() < 0.5:
             continue
-        print(f'Problems: {name}')  # print problems
+        print(f'Problems: {name}')
         health_of_spaceship -= damage
         print(f'current_health: {health_of_spaceship}')
         if health_of_spaceship <= 0:
