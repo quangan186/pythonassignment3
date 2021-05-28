d779db60674bbd560825a5cb870cdedc4b67aaf6
William <s3864077@rmit.edu.vn>
2021-05-28 11:14:19 +0700

    HD Spaceship 0.1 Final Note: Finish again

--- basic_info.py
+++ basic_info.py
@@ -21,7 +21,7 @@ def distance_and_time_cal(s):
     print(f'Distance between Earth and Mars: {s} km')
     while True:
         current_fuel_level = 100
-        time.sleep(10)
+        time.sleep(5)
         v = random.uniform(11, 2660)
         print(f'\nCurrent velocity:{v}')
         named_tuple = time.localtime()
@@ -37,12 +37,18 @@ def distance_and_time_cal(s):
         print(f'- Current distance from spaceship to Mars: {distance_from_Mars} km')
 
         # estimated time of arrival
-        estimate_time = ((s - v * delta_time_seconds) / v) / 3600  # hours
+        estimate_time = ((s - v * delta_time_seconds) / v) / 3600  # convert hours
         print(f'- Estimated time of arrival: {estimate_time} hours')
 
         current_fuel_level -= (fuel_burn_rate * distance_from_Earth)
         print(f'- Current fuel: {current_fuel_level} liters')
-
+        # Health of spaceship
+        time.sleep(5)
+        spaceship_health()
+        time.sleep(5)
+        # Health of crews
+        health_of_crew_members(3)
+        time.sleep(5)
         if distance_from_Earth >= s and distance_from_Mars <= 0:
             print("You have arrived")
             break
@@ -50,28 +56,18 @@ def distance_and_time_cal(s):
             print("You are on the way")
 
 
-def spaceship_health(problems):
+def spaceship_health():
     """
     the current condition of the spaceship
-    :param problems: any issues that the spaceship met
     :return:
     """
-    while True:
-        health_of_spaceship = 100
-        print(f"\nSpaceship health: {health_of_spaceship}")
-        time.sleep(10)
-        for name, damage in problems.items():
-            if random.random() < 0.5:
-                continue
-            print(f'\nProblems: {name}')
-            health_of_spaceship -= damage
-            print(f'Current_health: {health_of_spaceship}')
-            if health_of_spaceship > 0:
-                print("We are going to fix it")
-            else:
-                print("Your spaceship is stop working")
-                break
-        time.sleep(30)  # delay 30 secs for fixing spaceship
+    print("-----------Health of spaceship-------------")
+    health_of_spaceship = random.uniform(50, 100)
+    print(f"Spaceship health: {health_of_spaceship}")
+    if health_of_spaceship < 80:
+        print("The spaceship is damaged")
+    else:
+        print("The spaceship is working well")
 
 
 # Health condition of crew members
@@ -81,18 +77,13 @@ def health_of_crew_members(number_of_members):
     :param number_of_members:  number of people in the spaceship
     :return: none
     """
+    print("-----------Health of crew members-------------")
     health_problem_list = ['fever', 'sneeze', 'stomachache', 'sore throat']
-    while True:
-        victims = random.randrange(0, number_of_members)        # The amount of crew members who catch a disease
-
-        problem = random.choice(health_problem_list)            # Name of disease
-
-        if victims >= 2:
-            print(f'There are {number_of_members} total members and {victims} members have {problem}')
-
-        elif victims == 1:
-            print(f'There are {number_of_members} total members and {victims} member has {problem}')
-
-        else:
-            print(f'There are {number_of_members} total members and all of them are normal')
-        time.sleep(60)
+    victims = random.randrange(0, number_of_members)  # The amount of crew members who catch a disease
+    problem = random.choice(health_problem_list)  # Name of disease
+    if victims >= 2:
+        print(f'There are {number_of_members} total members and {victims} members have {problem}')
+    elif victims == 1:
+        print(f'There are {number_of_members} total members and {victims} member has {problem}')
+    else:
+        print(f'There are {number_of_members} total members and all of them are normal')
